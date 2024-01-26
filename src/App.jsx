f8e5bbc1ed0a79e31a04825e7513e2e228521fcb
dipanshu-codes <dipanshu.codes@gmail.com>
2024-01-26 14:56:02 +0530

    Modified routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
 import Landing from "./pages/Landing";
@@ -8,16 +8,47 @@ import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 
 export default function App() {
+  const token = JSON.parse(localStorage.getItem("user"));
+
   return (
     <>
       <Navbar />
       <div className="container mx-auto px-20">
         <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/dashboard" element={<Home />} />
-          <Route path="/profile" element={<Profile />} />
+          <Route
+            path="/"
+            element={
+              token ? <Navigate to={<Home />} /> : <Navigate to={<Landing />} />
+            }
+          />
+          <Route
+            path="/signup"
+            element={
+              token ? <Navigate to={<Home />} /> : <Navigate to={<Signup />} />
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              token ? <Navigate to={<Home />} /> : <Navigate to={<Login />} />
+            }
+          />
+          <Route
+            path="/dashboard"
+            element={
+              token ? <Navigate to={<Home />} /> : <Navigate to={<Login />} />
+            }
+          />
+          <Route
+            path="/profile"
+            element={
+              token ? (
+                <Navigate to={<Profile />} />
+              ) : (
+                <Navigate to={<Login />} />
+              )
+            }
+          />
         </Routes>
       </div>
     </>
